c9094d615b2ddf5944d4bace3bc26c50ab190841
wwff1 <boss.lq71@mail.ru>
2021-06-22 23:50:23 +0500

    12132

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use('/api/order', require('./routes/order.routes'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-    app.get('*', (req, res) =>{
+
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
